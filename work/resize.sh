e677783e9bba90c6efb63f1fea0d268abc509fb2
jtamu <gtamu.dip@gmail.com>
2024-08-02 16:56:15 +0900

    feat: ファイル名にスペースが含まれている場合の考慮

--- work/resize.sh
+++ work/resize.sh
@@ -4,11 +4,11 @@ size=$SIZE
 ext=$EXT
 file=$1
 
-if [[ ! $file =~ $ext ]]; then
+if [[ ! "$file" =~ $ext ]]; then
 	exit 0
 fi
 
-convert -resize $size $file tmp
-mv tmp $file
+convert -resize $size "$file" tmp
+mv tmp "$file"
 
 echo "$file resizing completed!"
